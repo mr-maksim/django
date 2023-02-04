7a05a35fcbe75fe3cb7afc6dcd90e84d85b04520
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2023-02-04 20:08:46 +0300

    Edit views

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -6,10 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    obj_list = Student.objects.all()
+    context = {
+        'object_list': obj_list
+    }
     ordering = 'group'
 
     return render(request, template, context)
