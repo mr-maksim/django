437afb74317cd17234f24a71cb6b6b5ab5931c85
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2023-02-04 15:56:27 +0300

    Edit model

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.CharField(max_length=255)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=255, unique=True,
+                            db_index=True, verbose_name="URL")
+    
