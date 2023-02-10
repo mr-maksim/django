a48c4c5b423902bfd42acedbac369602454003ff
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2023-02-10 07:22:28 +0300

    Added urls

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', CreateAPIView.as_view()),
+    path('sensors/<pk>/', RetrieveUpdateAPIView.as_view()),
+    path('list/', ListView.as_view()),
+    path('measurements/', ListCreateAPIView.as_view()),
 ]
