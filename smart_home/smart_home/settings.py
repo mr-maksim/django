77b680d381f11014584e40a3af682f5575a9b8ed
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2023-02-10 07:16:33 +0300

    Edit database path

--- smart_home/smart_home/settings.py
+++ smart_home/smart_home/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_smart_home',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
