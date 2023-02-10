911960354f79297b3b8bf031e3041b070cb0eaca
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2023-02-10 07:32:57 +0300

    Final changes

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from measurement.views import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView, ListView
 
 urlpatterns = [
     path('sensors/', CreateAPIView.as_view()),
